128ed3769c7e3c5580d0e5bcdd0469e3e7b95fbc
Ale <156391201+AleRodriguezCruz@users.noreply.github.com>
2024-03-20 18:38:40 -0700

    se agrego los dos puntos y su valor =58

--- Practica 1.5/Practica1.5.py
+++ Practica 1.5/Practica1.5.py
@@ -1,54 +1,32 @@
 import json
 
-# Función para leer los valores del archivo tokens.txt
-def leer_archivo_tokens(ruta_del_archivo):
-    valores_simbolos = {}
-    with open(ruta_del_archivo, encoding='utf-8') as f:
-        for linea in f:
-            campos = linea.strip().split(" ")
-            if len(campos) > 1:
-                clave, valor = campos[:2]
-                try:
-                    int_valor = int(valor)
-                    valores_simbolos[clave] = int_valor
-                except ValueError:
-                    valores_simbolos[clave] = valor
-    return valores_simbolos
+# Función para asignar tokens Unicode a los caracteres del contenido, asignando 58 a los dos puntos ':'
+def tokenizar_contenido(contenido):
+    tokens = {}
+    for caracter in contenido:
+        if caracter == ':':
+            tokens[caracter] = 58  # Asignar el valor 58 a los dos puntos ':'
+        else:
+            valor = ord(caracter)
+            tokens[caracter] = valor
+    return tokens
 
-# Leer los valores del archivo tokens.txt
-valores_simbolos = leer_archivo_tokens('tokens.txt')
-
-# Convertir el archivo JSON a un diccionario
+# Leer el contenido del archivo JSON
 with open('ejemplo.json') as file:
     data = json.load(file)
+    contenido = json.dumps(data)
+
+# Asignar tokens a cada carácter del contenido JSON
+tokens = tokenizar_contenido(contenido)
 
 # Construir el archivo de salida 
 with open('resultado.txt', 'w') as file:
-    file.write('{" : 123\n')
-    file.write('"":34\n')
-    
-    for key, value in data.items():
-        for char in key:  # Procesar cada carácter de la clave
-            if char == ' ':
-                file.write('(Espacio) : 32\n')
-            elif char == '\n':
-                file.write('(Enter) : 10\n')
-            else:
-                file.write(f'"{char}" : {ord(char)}\n')
-        
-        file.write('"":34\n')
-
-        if isinstance(value, str):
-            for char in value:  # Procesar cada carácter del valor si es una cadena
-                if char == ' ':
-                    file.write('(Espacio) : 32\n')
-                elif char == '\n':
-                    file.write('(Enter) : 10\n')
-                else:
-                    file.write(f'"{char}" : {ord(char)}\n')
-
-        file.write('"":34\n')
-
-    file.write('"}" : 125\n')
+    for char in contenido:
+        if char == ' ':
+            file.write('(Espacio) : 32\n')
+        elif char == '\n':
+            file.write('(Enter) : 10\n')
+        else:
+            file.write(f'"{char}" : {tokens[char]}\n')
 
 print("Proceso completado. Los datos del archivo JSON se han guardado en resultado.txt ")
