d5470aeabd3a4327a27c5d4fae3812c7933c150c
Alejandra <al22760049@ite.edu.mx>
2024-03-23 21:25:54 -0700

    Se agrego descripcion a las funciones

--- Practica 2/Evaluador.py
+++ Practica 2/Evaluador.py
@@ -4,58 +4,58 @@
 #No_Control:22760049
 
 # Función para leer el contenido de un archivo
-def read_file(file_name):
-    with open(file_name, 'r') as file:
-        return file.read()
+def leer_archivo(nombre_archivo):
+    with open(nombre_archivo, 'r') as archivo:
+        return archivo.read()
 
 # Función para tokenizar el contenido del archivo
-def tokenize_content(content):
-    tokens_list = [ord(char) for char in content]  # Convierte cada carácter del contenido en su valor ASCII
-    return tokens_list
+def tokenizar_contenido(contenido):
+    lista_tokens = [ord(caracter) for caracter in contenido]  # Convierte cada carácter del contenido en su valor ASCII
+    return lista_tokens
 
 # Función para mostrar el contenido del archivo junto con sus tokens
-def show_content_tokens(content, tokens):
-    for char in content:
-        if char == '\n':
-            print(f'{tokens[char]}=[Enter]')
-        elif char == ' ':
-            print(f'{tokens[char]}=[SPACE]')
+def mostrar_contenido_tokens(contenido, tokens):
+    for caracter in contenido:
+        if caracter == '\n':
+            print(f'{tokens[caracter]}=[Enter]')
+        elif caracter == ' ':
+            print(f'{tokens[caracter]}=[Espacio]')
         else:
-            print(f'{tokens[char]}={char}')
+            print(f'{tokens[caracter]}={caracter}')
 
 # Función para escribir el contenido del archivo con sus tokens en un nuevo archivo
-def write_document(content, tokens):
-    with open('salida.txt', 'w') as output:
-        for char in content:
-            if char == '\n':
-                output.write(f'{tokens[char]}=[ENTER]\n')
-            elif char == ' ':
-                output.write(f'{tokens[char]}=[SPACE]\n')
+def escribir_documento(contenido, tokens):
+    with open('salida.txt', 'w') as salida:
+        for caracter in contenido:
+            if caracter == '\n':
+                salida.write(f'{tokens[caracter]}=[ENTER]\n')
+            elif caracter == ' ':
+                salida.write(f'{tokens[caracter]}=[ESPACIO]\n')
             else:
-                output.write(f'{tokens[char]}={char}\n')
+                salida.write(f'{tokens[caracter]}={caracter}\n')
 
 # Función para verificar si el archivo es un JSON válido
-def is_json(tokens):
+def es_json(tokens):
     if tokens[0] != 123:  # Verifica si el primer token es el de la llave de apertura '{'
         print('El archivo no es un JSON válido.')
         exit()
 
 # Función para agrupar los tokens de las cadenas de caracteres en un solo token
-def tokenize_string(tokens):
-    grouped_tokens = []
-    in_string = False
+def tokenizar_cadena(tokens):
+    tokens_agrupados = []
+    en_cadena = False
     for token in tokens:
         if (token >= 65 and token <= 90) or (token >= 97 and token <= 122):
-            if not in_string:
-                grouped_tokens.append(200)  # Añade un token especial para indicar inicio de cadena
-                in_string = True
+            if not en_cadena:
+                tokens_agrupados.append(200)  # Añade un token especial para indicar inicio de cadena
+                en_cadena = True
         else:
-            in_string = False
-            grouped_tokens.append(token)
-    return grouped_tokens
+            en_cadena = False
+            tokens_agrupados.append(token)
+    return tokens_agrupados
 
 # Función para verificar si todas las comillas en el archivo están cerradas correctamente
-def is_string(tokens):
+def es_cadena(tokens):
     flag = True 
     for token in tokens:
         if token == 34:  # Verifica si el token corresponde a una comilla doble '"'
@@ -67,24 +67,24 @@ def is_string(tokens):
 
 # Función principal del programa
 def main():
-    file_content = read_file('ejemplo.json')  # Lee el contenido del archivo 'ejemplo.json'
-    tokens = tokenize_content(file_content)   # Tokeniza el contenido del archivo
+    contenido_archivo = leer_archivo('ejemplo.json')  # Lee el contenido del archivo 'ejemplo.json'
+    tokens = tokenizar_contenido(contenido_archivo)   # Tokeniza el contenido del archivo
     
-    is_json(tokens)  # Verifica si el archivo es un JSON válido
+    es_json(tokens)  # Verifica si el archivo es un JSON válido
     
-    tokenize_string(tokens)  # Agrupa los tokens de las cadenas de caracteres
+    tokenizar_cadena(tokens)  # Agrupa los tokens de las cadenas de caracteres
     
-    show_content_tokens(file_content, {char: ord(char) for char in file_content})  # Muestra el contenido del archivo con sus tokens
+    mostrar_contenido_tokens(contenido_archivo, {caracter: ord(caracter) for caracter in contenido_archivo})  # Muestra el contenido del archivo con sus tokens
     
-    write_document(file_content, {char: ord(char) for char in file_content})  # Escribe el contenido del archivo con sus tokens en un nuevo archivo
+    escribir_documento(contenido_archivo, {caracter: ord(caracter) for caracter in contenido_archivo})  # Escribe el contenido del archivo con sus tokens en un nuevo archivo
     
-    grouped_tokens = tokenize_string(tokens)  # Obtiene los tokens agrupados de las cadenas de caracteres
+    tokens_agrupados = tokenizar_cadena(tokens)  # Obtiene los tokens agrupados de las cadenas de caracteres
     
     print(f'Tokens: {tokens}')  # Imprime los tokens originales
     
-    print(grouped_tokens)  # Imprime los tokens agrupados
+    print(tokens_agrupados)  # Imprime los tokens agrupados
 
-    if not is_string(tokens):  # Verifica si todas las comillas están cerradas correctamente si no no es un string
+    if not es_cadena(tokens):  # Verifica si todas las comillas están cerradas correctamente si no no es un string
         print("ERROR: Las comillas no se cerraron correctamente")
 
 if __name__ == '__main__':
